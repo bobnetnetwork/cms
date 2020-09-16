801c5a79d8f8cda4dbd1984c4ee4c0ca09bd4e99
Bobesz <bob@bobnet.network>
2020-09-17 00:43:29 +0200

    Update DashboardServerService.ts

--- src/service/DashboardServerService.ts
+++ src/service/DashboardServerService.ts
@@ -1,10 +1,7 @@
 import { ServerService } from './ServerService.js';
 import express, {Express} from "express";
-import {DashboardRouter} from "../routes/dashboard/DashboardRouter.js";
 import mustacheExpress from "mustache-express";
 import path from 'path';
-import fetch from "node-fetch";
-import http from "http";
 import axios from "axios";
 import {Article} from "../model/content/Article.js"
 import {DashboardRoutesEnum} from "../messages/enums/DashboardRoutesEnum.js";
@@ -20,11 +17,8 @@ export class DashboardServerService extends ServerService {
         this.app.set('view engine', 'mustache');
 
         this.app.get('/', function (req, res) {
-            
             res.render('index', { name : 'Luk'});
         });
-
-
 
         this.app.get('/valami', function(req, res) {
             res.render('valami', {valami: 'valamik'});
@@ -36,16 +30,11 @@ export class DashboardServerService extends ServerService {
             try {
                 const response = await axios.get(url);
                 var cikkek:[Article] = response.data.content;
-
                 res.render('cikkek', { articles: cikkek});
-
-
             } catch (exception) {
                 process.stderr.write(`ERROR received from ${url}: ${exception}\n`);
             }
         });
-
-
     }
 
     public openConnections(): void {
