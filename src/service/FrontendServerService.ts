266d5c4e8b3cfa6c8889905a3bb253788cc266b2
Bobesz <bob@bobnet.network>
2020-09-18 12:22:18 +0200

    frontend template changed to dep (startbootstrap-modern-business)

--- src/service/FrontendServerService.ts
+++ src/service/FrontendServerService.ts
@@ -1,10 +1,17 @@
 import { ServerService } from './ServerService.js';
-import {Express} from "express";
+import express, {Express} from "express";
+import path from 'path';
+import mustacheExpress from "mustache-express";
+
 
 export class FrontendServerService extends ServerService {
 
     constructor(app: Express) {
         super("FrontendServerService", app);
+        this.app.engine('mustache', mustacheExpress());
+        this.app.set('views', path.resolve()+ '\\src\\views\\frontend');
+        this.app.use(express.static(path.resolve()+ '\\node_modules\\startbootstrap-modern-business'));
+        this.app.set('view engine', 'mustache');
     }
 
     public openConnections(): void {
